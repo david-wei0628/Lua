833f748644a00c2f13ab0228e906ced469cd4ea2
david-wei0628 <62085333+david-wei0628@users.noreply.github.com>
2024-04-16 17:12:04 +0800

    addressable txt文本問題
    TXT文本存取在StreamingAssets
    會因為存取權限狀態
    讓LoadAssetAsync<TextAsset> 無法在資料包內正常讀取
    如果是存在StreamingAssets
    要用Addressables.LoadResourceLocationsAsync 及addressable Key
    來尋找指定文件

--- Assets/Editor/BuildTargetChecker.cs
+++ Assets/Editor/BuildTargetChecker.cs
@@ -2,6 +2,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEditor.AddressableAssets;
 using UnityEditor.AddressableAssets.Settings;
+using System.IO;
 
 public class BuildTargetChecker
 {
@@ -11,13 +12,39 @@ public class BuildTargetChecker
         //BuildTarget activeTarget = EditorUserBuildSettings.activeBuildTarget;
         //Debug.Log("當前的BuildTarget是：" + activeTarget.ToString());
 
-        var settings = AddressableAssetSettingsDefaultObject.Settings;
+        //var settings = AddressableAssetSettingsDefaultObject.Settings;
+        //var path = AddressableAssetSettings.kRemoteBuildPathValue;
+        //Debug.Log(path);
+        //// 檢查buildTarget
+        ////var buildTarget = settings.activeProfileSettings.GetBuildTarget(settings.activeProfileId);
+        //var buildTarget = settings.activeProfileId;
 
-        // 檢查buildTarget
-        //var buildTarget = settings.activeProfileSettings.GetBuildTarget(settings.activeProfileId);
-        var buildTarget = settings.activeProfileId;
-       
-        // 輸出當前的buildTarget
-        Debug.Log("當前的Build Target是: " + buildTarget);
+        //// 輸出當前的buildTarget
+        //Debug.Log("當前的Build Target是: " + buildTarget);
+
+        // 獲取當前活動的 Profile ID
+        string activeProfileId = AddressableAssetSettingsDefaultObject.Settings.activeProfileId;
+
+        // 使用 Profile ID 來獲取 BuildTarget 名稱
+        string buildTargetName = AddressableAssetSettingsDefaultObject.Settings.profileSettings.GetValueByName(activeProfileId, "BuildTarget");
+        Debug.Log(buildTargetName);
+
+    }
+
+    [MenuItem("Build/Perform Build")]
+    public static void PerformBuild()
+    {
+        // 獲取當前的BuildTarget
+        //string buildTargetName = EditorUserBuildSettings.activeBuildTarget.ToString();
+
+        // 獲取當前活動的 Profile ID
+        string activeProfileId = AddressableAssetSettingsDefaultObject.Settings.activeProfileId;
+
+        // 使用 Profile ID 來獲取 BuildTarget 名稱
+        string buildTargetName = AddressableAssetSettingsDefaultObject.Settings.profileSettings.GetValueByName(activeProfileId, "BuildTarget");
+        // 將BuildTarget名稱保存到一個文本文件中
+        File.WriteAllText("Assets/StreamingAssets/BuildTargetName.txt", buildTargetName);
+
+        // 其他構建操作...
     }
 }
--- Assets/Script/AddressCS/AddressableDataCS.cs
+++ Assets/Script/AddressCS/AddressableDataCS.cs
@@ -1,32 +1,80 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AddressableAssets;
 using UnityEngine.AddressableAssets.Initialization;
 using UnityEngine.AddressableAssets.ResourceLocators;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using System.IO;
+using System.Collections.Generic;
 
 public class AddressableDataCS : MonoBehaviour
 {
     public Text Lable;
     public string newLocalPath;
+    public string A;
+    string dataass = "BuildTargetNameN";
+
     public void Data_BTN()
     {
-        //string buildTargetValue = AddressableRuntimeProperties.GetPropertyValue("BuildTargetValue");
-        //AddressablesRuntimeProperties.SetPropertyValue("BuildTragetValue", "ScencePrfab2");
-        ///*string buildTargetValue = */AddressablesRuntimeProperties.SetPropertyValue("BuildTarget","VV");
-        //print(buildTargetValue);
-        string runtimePath = Addressables.RuntimePath;
-        string buildTargetName = AddressablesRuntimeProperties.EvaluateProperty("BuildTarget");
-        string fullPath = $"{runtimePath}/{buildTargetName}";
-        Debug.Log(buildTargetName);
-        ResourceManagerRuntimeData runtimeData = new ResourceManagerRuntimeData();
-        string A = runtimeData.BuildTarget;
-        print(A);
-        //Addressables.InitializeAsync().Completed += OnAddressablesInitialized;
+        ReadAddressableBuildTarget();
     }
 
+    void ReadAddressableBuildTarget()
+    {
+        Addressables.LoadAssetAsync<TextAsset>(dataass).Completed += handle =>
+        {
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                // 獲取文本內容，即buildTarget名稱
+                //string buildTargetName = handle.Result.text;
+                //Debug.Log("Current Build Target: " + buildTargetName);
+                Debug.Log("A");
+            }
+        };
+        //Addressables.LoadResourceLocationsAsync(dataass).Completed += handle =>
+        //{
+        //    if (handle.Status == AsyncOperationStatus.Succeeded)
+        //    {
+        //        // 加載成功，處理資源位置
+        //        var locations = handle.Result;
+        //        foreach (var location in locations)
+        //        {
+        //            Debug.Log($"找到資源: {location.PrimaryKey}");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        // 加載失敗，處理錯誤
+        //        Debug.LogError("無法加載資源位置。");
+        //    }
+        //};
+    }
+
+    /* ////string buildTargetValue = AddressableRuntimeProperties.GetPropertyValue("BuildTargetValue");
+     ////AddressablesRuntimeProperties.SetPropertyValue("BuildTragetValue", "ScencePrfab2");
+     /////*string buildTargetValue = //AddressablesRuntimeProperties.SetPropertyValue("BuildTarget","VV");
+     ////print(buildTargetValue);
+     //string runtimePath = Addressables.RuntimePath;
+     //string buildTargetName = AddressablesRuntimeProperties.EvaluateProperty("BuildTarget");
+     //string fullPath = $"{runtimePath}/{buildTargetName}";
+     //string buildTarget = AddressablesRuntimeProperties.EvaluateProperty("[BuildTarget]");
+     //ResourceManagerRuntimeData runtimeData = new ResourceManagerRuntimeData();
+
+     //Addressables.InitializeAsync().Completed += OnAddressablesInitialized;*/
+
+    //void LoadActiveProfileId()
+    //{
+    //    // 假?您已?在构建?程中?建了一?包含Profile ID的文本文件，并?其放在Resources文件?中
+    //    TextAsset profileIdText = Resources.Load<TextAsset>("ProfileID");
+    //    if (profileIdText != null)
+    //    {
+    //        newLocalPath = profileIdText.text;
+    //    }
+    //    else
+    //    {
+    //        Debug.LogError("未能加?Profile ID!");
+    //    }
+    //}
 
     /*void OnAddressablesInitialized(AsyncOperationHandle<IResourceLocator> obj)
     {
